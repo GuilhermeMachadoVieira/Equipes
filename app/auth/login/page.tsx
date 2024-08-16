772f3031c0b710a8c79c6b5d11e36f4bc16ebc02
GuilhermeMachadoVieira <guilherme.vieira25@fatec.sp.gov.br>
2024-08-16 19:39:23 -0300

    certinho

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -3,7 +3,6 @@
 
 import { useState } from 'react';
 import Link from 'next/link';
-
 import '../../globals.css';
 
 export default function LoginPage() {
@@ -15,11 +14,15 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="flex min-h-screen">
+    <div className="flex min-h-screen"
+    style={{ backgroundImage: 'url(/fatec-sorocaba.jpeg)' }}
+    >
       {/* Left Section with Welcome Text */}
-      <div className="flex-1 relative bg-cover bg-center" style={{ backgroundImage: 'url(/school-bg.jpg)' }}>
+      <div
+        className="flex-1 relative bg-cover bg-center"
+      >
         <div className="absolute inset-0 bg-black bg-opacity-40" />
-        <div className="relative z-10 flex flex-col justify-center items-start p-10 text-white">
+        <div className="relative z-10 flex flex-col justify-center items-start p-10 text-white h-full">
           <h1 className="text-5xl font-bold mb-4">Olá, professor!</h1>
           <p className="text-xl">Faça seu login ou cadastro</p>
           <p className="text-xl">para acessar todas</p>
@@ -28,11 +31,11 @@ export default function LoginPage() {
       </div>
 
       {/* Right Section with Login Form */}
-      <div className="flex-1 bg-blue-500 bg-opacity-70 flex flex-col justify-center items-center p-10">
-        <div className=" p-8 rounded-lg shadow-md w-full max-w-md">
+      <div className="w-full max-w-md flex-none bg-blue-900 bg-opacity-80 flex flex-col backdrop-blur justify-center items-center p-10">
+        <div className="p-8 rounded-lg  w-full max-w-md">
           {/* Logo */}
           <div className="flex justify-center mb-6">
-            <img src="/teacher-logo.png" alt="Teacher Web Logo" className="w-32" />
+            <img src="\escrita_preta_logo.png" alt="Teacher Web Logo" className="w-56" />
           </div>
 
           {/* Login Form */}
@@ -43,7 +46,7 @@ export default function LoginPage() {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 required
-                className="w-full p-2 bg-transparent border-b-2 border-white rounded-none focus:outline-none focus:border-blue-500 placeholder-white"
+                className="w-full p-2 bg-transparent border-b-2 border-white rounded-none text-white focus:outline-none focus:border-blue-500 placeholder-white"
                 placeholder="E-mail"
               />
             </div>
@@ -54,15 +57,15 @@ export default function LoginPage() {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 required
-                className="w-full p-2 bg-transparent border-b-2 border-white rounded-none focus:outline-none focus:border-blue-500 placeholder-white"
+                className="w-full p-2 bg-transparent border-b-2 border-white rounded-none text-white  focus:outline-none focus:border-blue-500 placeholder-white"
                 placeholder="Senha"
               />
-              <Link href="#" className="absolute right-0 top-full mt-6 text-sm text-blue-500">Esqueci minha senha</Link>
+              <Link href="#" className="absolute right-0 top-full mt-6 text-sm text-white">Esqueci minha senha</Link>
             </div>
 
             <button
               type="submit"
-              className="w-full bg-white text-black p-2 rounded hover:bg-blue-600 transition"
+              className="w-full bg-white text-black p-2 rounded-full rounded hover:bg-gray-300 transition"
             >
               Entrar
             </button>
@@ -71,11 +74,11 @@ export default function LoginPage() {
           {/* Create Account and Google Login */}
           <div className="mt-6 flex flex-col space-y-4">
             <Link href="/auth/register">
-              <button className="w-full bg-white text-black p-2 rounded hover:bg-gray-300 transition">
+              <button className="w-full bg-white rounded-full text-black p-2 rounded hover:bg-gray-300 transition">
                 Criar Conta
               </button>
             </Link>
-            <button className="w-full flex items-center justify-center bg-black text-white p-2 rounded hover:bg-gray-300 transition">
+            <button className="w-full flex items-center rounded-full justify-center bg-black text-white p-2 rounded hover:bg-gray-300 transition">
               <img src="/google-logo.png" alt="Google Logo" className="w-5 h-5 mr-2" />
               ENTRAR COM GOOGLE
             </button>
